e3bd975045a37d8ccec27051b1cd7288811a6be7
Hchap1 <58793111+hchap1@users.noreply.github.com>
2024-08-08 12:44:31 +1000

    Added new motors ect.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Motors.java
@@ -17,6 +17,10 @@ public class Motors {
     public Motor leftIntake;
     public Motor rightIntake;
 
+    // Outake motors
+    public Motor leftSlide;
+    public Motor rightSlide;
+
     public Motors(HardwareMap hardwareMap) {
         // FTC Lib motor wrappers1
         leftFront = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.leftFront);
@@ -25,6 +29,8 @@ public class Motors {
         rightBack = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.rightBack);
         leftIntake = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.leftIntake);
         rightIntake = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.rightIntake);
+        leftSlide = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.leftSlide);
+        rightSlide = new Motor(hardwareMap, RobotParameters.Motors.HardwareMapNames.rightSlide);
         // Set motor directions
         leftFront.setInverted(RobotParameters.Motors.Reversed.leftFront);
         leftBack.setInverted(RobotParameters.Motors.Reversed.leftBack);
@@ -32,6 +38,8 @@ public class Motors {
         rightBack.setInverted(RobotParameters.Motors.Reversed.rightBack);
         leftIntake.setInverted(RobotParameters.Motors.Reversed.leftIntake);
         rightIntake.setInverted(RobotParameters.Motors.Reversed.rightIntake);
+        leftSlide.setInverted(RobotParameters.Motors.Reversed.leftSlide);
+        rightSlide.setInverted(RobotParameters.Motors.Reversed.rightSlide);
         // Set braking behaviour
         leftFront.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
         leftBack.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
@@ -39,5 +47,7 @@ public class Motors {
         rightBack.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
         leftIntake.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
         rightIntake.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
+        leftSlide.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
+        rightSlide.setZeroPowerBehavior(RobotParameters.Motors.zeroPowerBehaviour);
     }
 }
