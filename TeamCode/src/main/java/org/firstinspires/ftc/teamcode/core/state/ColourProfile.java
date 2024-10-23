05383947baa377431cd3173a8723a68dfcdf1dc5
hchap1 <hchap1@hotmail.com>
2024-10-23 22:27:22 +1100

    changed colour profiling to update better & moved some code around

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/state/ColourProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/state/ColourProfile.java
@@ -15,22 +15,27 @@ public class ColourProfile {
     public double r;
     public double g;
     public double b;
+    public double d;
 
     public ColourProfile(Sensors sensors) {
-        update(sensors);
-        if (r > (b + g) * colourFilter) { redCount += 1; }
-        else { redCount = 0; }
-        if (b > (r + g) * colourFilter) { blueCount += 1; }
-        else { blueCount = 0; }
-        if (b * 2.0 < (r + g) && g > r * colourFilter) { yellowCount += 1; }
-        else { yellowCount = 0; }
-        if (sensors.d() < RobotParameters.Thresholds.intakeSamplePresent) { proxCount += 1; }
+        r = sensors.r();
+        g = sensors.g();
+        b = sensors.b();
+        d = sensors.d();
     }
 
     public void update(Sensors sensors) {
         r = sensors.r();
         g = sensors.g();
         b = sensors.b();
+        d = sensors.d();
+        if (r > (b + g) * colourFilter) { redCount += 1; }
+        else { redCount = 0; }
+        if (b > (r + g) * colourFilter) { blueCount += 1; }
+        else { blueCount = 0; }
+        if (b * 2.0 < (r + g) && g > r * colourFilter) { yellowCount += 1; }
+        else { yellowCount = 0; }
+        if (d < RobotParameters.Thresholds.intakeSamplePresent) { proxCount += 1; }
     }
 
     public Colour classify() {
