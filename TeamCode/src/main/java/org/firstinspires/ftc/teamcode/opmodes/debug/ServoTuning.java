5c730a46bba1e66bf10d078868cef1336aad53ec
Harrison Chapman <hchap1@hotmail.com>
2024-10-24 11:17:59 +1000

    finished splitting original tuning opmode into colour profile and servo position.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/debug/ServoTuning.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.core.Sensors;
+import org.firstinspires.ftc.teamcode.core.Servos;
+import org.firstinspires.ftc.teamcode.core.params.RobotParameters;
+import org.firstinspires.ftc.teamcode.core.state.Colour;
+import org.firstinspires.ftc.teamcode.core.state.ColourProfile;
+
+@TeleOp
+public class ServoTuning extends LinearOpMode
+{
+    @Config
+    public static class Tune {
+        public static double intakeSpinPower = 0.0;
+        public static double intakeLiftPosition = 0.0;
+        public static double outtakeArmPosition = 0.0;
+        public static double outtakeClawPosition = 0.0;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        Servos servos = new Servos(hardwareMap);
+        Sensors sensors = new Sensors(hardwareMap);
+
+        //////////////////////////////////////////
+        // Runs when the init button is pressed //
+        //////////////////////////////////////////
+
+        if (isStopRequested()) return;
+        waitForStart();
+
+        //////////////////////////////////////////
+        // Runs when the play button is pressed //
+        //////////////////////////////////////////
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        // Main loop
+        while (opModeIsActive()) {
+            servos.intakeServoA.set(Tune.intakeSpinPower);
+            servos.intakeServoB.set(Tune.intakeSpinPower);
+            servos.rightIntakeLiftServo.setPosition(Tune.intakeLiftPosition);
+            servos.leftIntakeLiftServo.setPosition(1.0 - Tune.intakeLiftPosition);
+            servos.armServo.setPosition(Tune.outtakeArmPosition);
+            servos.bucketServo.setPosition(Tune.outtakeClawPosition);
+        }
+    }
+}
