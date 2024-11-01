a9b4b2c8ccac05115e3890f08a352aaee7cc059a
Marley Reeves <marley@marlstar.com>
2024-11-01 19:20:50 +1000

    Main autonomous - Finish implementing the main autonomous opmode - Given that I'm home, untested. Hopefully this works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/Paths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/Paths.java
@@ -4,11 +4,14 @@ import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
 
 public class Paths {
     public static PathChain start_to_basket = org.firstinspires.ftc.teamcode.auto.paths.StartToBasket.path();
+    public static PathChain outtake_to_park = org.firstinspires.ftc.teamcode.auto.paths.OuttakeToPark.path();
 
     public static class yellow_spike {
         public static PathChain BOTTOM = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.BOTTOM.path();
+        public static PathChain BOTTOM_intake = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.BOTTOM_intake.path();
         public static PathChain BOTTOM_return = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.BOTTOM_return.path();
         public static PathChain MIDDLE = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.MIDDLE.path();
+        public static PathChain MIDDLE_intake = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.MIDDLE_intake.path();
         public static PathChain MIDDLE_return = org.firstinspires.ftc.teamcode.auto.paths.yellow_spike.MIDDLE_return.path();
     }
 }
