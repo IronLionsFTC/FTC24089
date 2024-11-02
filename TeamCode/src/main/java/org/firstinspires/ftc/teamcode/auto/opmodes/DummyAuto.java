cfccfed7df4a45bd65e8d429e7e9b4fa74036f00
Marley Reeves <marley@marlstar.com>
2024-11-02 13:04:09 +1000

    Dummy autonomous opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/opmodes/DummyAuto.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.auto.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
+
+// This will start in the corner for blue and go from there
+// Start facing toward the baskets with the center on the 45 degree angled corner of the human player area
+@Autonomous(name = "Dummy Auto", group = "_MAIN_")
+public class DummyAuto extends OpMode {
+    @Override
+    public void init() {
+        return;
+    }
+
+    @Override
+    public void start() {
+        return;
+    }
+
+    @Override
+    public void loop() {
+        return;
+    }
+}
