71fb25d700f41d15dfea449e778b1c1fa308f319
Marley Reeves <marley@marlstar.com>
2024-11-03 13:22:07 +1000

    FINALS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/FINALS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/FINALS.java
@@ -15,35 +15,89 @@ public class FINALS {
                         // Line 1
                         new BezierLine(
                                 new Point(9.000, 87.000, Point.CARTESIAN),
-                                new Point(15.000, 110.000, Point.CARTESIAN)
+                                new Point(15.729, 112.985, Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(90))
                 .addPath(
                         // Line 2
                         new BezierCurve(
-                                new Point(15.000, 110.000, Point.CARTESIAN),
-                                new Point(19.052, 123.397, Point.CARTESIAN),
-                                new Point(13.292, 130.265, Point.CARTESIAN)
+                                new Point(15.729, 112.985, Point.CARTESIAN),
+                                new Point(17.058, 124.505, Point.CARTESIAN),
+                                new Point(13.735, 129.600, Point.CARTESIAN)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(135))
                 .addPath(
                         // Line 3
-                        new BezierLine(
-                                new Point(13.292, 130.265, Point.CARTESIAN),
-                                new Point(12.849, 62.252, Point.CARTESIAN)
+                        new BezierCurve(
+                                new Point(13.735, 129.600, Point.CARTESIAN),
+                                new Point(23.926, 111.212, Point.CARTESIAN),
+                                new Point(36.332, 108.111, Point.CARTESIAN)
                         )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(90))
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
                 .addPath(
                         // Line 4
-                        new BezierLine(
-                                new Point(12.849, 62.252, Point.CARTESIAN),
-                                new Point(9.969, 35.889, Point.CARTESIAN)
+                        new BezierCurve(
+                                new Point(36.332, 108.111, Point.CARTESIAN),
+                                new Point(66.240, 104.123, Point.CARTESIAN),
+                                new Point(60.037, 120.074, Point.CARTESIAN)
                         )
                 )
-                .setConstantHeadingInterpolation(Math.toRadians(90));
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .addPath(
+                        // Line 5
+                        new BezierCurve(
+                                new Point(60.037, 120.074, Point.CARTESIAN),
+                                new Point(27.914, 117.637, Point.CARTESIAN),
+                                new Point(13.735, 129.600, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                .addPath(
+                        // Line 6
+                        new BezierCurve(
+                                new Point(13.735, 129.600, Point.CARTESIAN),
+                                new Point(50.068, 106.338, Point.CARTESIAN),
+                                new Point(89.945, 123.397, Point.CARTESIAN),
+                                new Point(54.277, 129.822, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                .addPath(
+                        // Line 7
+                        new BezierLine(
+                                new Point(54.277, 129.822, Point.CARTESIAN),
+                                new Point(13.735, 129.600, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                .addPath(
+                        // Line 8
+                        new BezierLine(
+                                new Point(13.735, 129.600, Point.CARTESIAN),
+                                new Point(32.788, 35.446, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(0))
+                .addPath(
+                        // Line 9
+                        new BezierCurve(
+                                new Point(32.788, 35.446, Point.CARTESIAN),
+                                new Point(46.745, 38.548, Point.CARTESIAN),
+                                new Point(45.858, 30.351, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-90))
+                .addPath(
+                        // Line 10
+                        new BezierLine(
+                                new Point(45.858, 30.351, Point.CARTESIAN),
+                                new Point(10.634, 34.117, Point.CARTESIAN)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(-90), Math.toRadians(-180));
         return builder.build();
     }
 }
