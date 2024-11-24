c9ab7730e6f81d2f64344f6ca3bde57c044404e8
hchap1 <hchap1@hotmail.com>
2024-11-24 20:54:40 +1100

    decided to actually copy code over to new commands rather than just calling autonomous functions, because some of them are polling only with no setting counterpart, so its better to split them up rather than jankily peforming state logic in the isFinished of the command.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GrabGameObjectWithIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/GrabGameObjectWithIntake.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.commands;
 import com.arcrobotics.ftclib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.auto.AutonomousRobot;
+import org.firstinspires.ftc.teamcode.core.state.intake.IntakeState;
 
 public class GrabGameObjectWithIntake extends CommandBase {
     private AutonomousRobot robot;
@@ -16,11 +17,33 @@ public class GrabGameObjectWithIntake extends CommandBase {
 
     @Override
     public void execute() {
-
+        if (robot.intakeTimer.getElapsedTimeSeconds() > 0.6 && robot.robot.state.intake.intakeState == IntakeState.ExtendedClawDown) robot.closeIntakeClaw();
+        if (robot.intakeTimer.getElapsedTimeSeconds() > 1.0 && robot.robot.state.intake.intakeState == IntakeState.ExtendedGrabbingOffWallClawOpen) robot.closeIntakeClaw();
+        if (robot.intakeTimer.getElapsedTimeSeconds() > 1.0 && robot.robot.state.intake.intakeState == IntakeState.Grabbing) {
+            if (robot.robot.state.intake.foldIntakeBeforeRetraction.getElapsedTimeSeconds() > 2.6) {
+                robot.robot.state.intake.foldIntakeBeforeRetraction.resetTimer();
+            }
+            robot.robot.state.intake.intakeState = IntakeState.Transfer;
+        }
+        if (robot.intakeTimer.getElapsedTimeSeconds() > 2.0 && robot.robot.state.intake.intakeState == IntakeState.ExtendedGrabbingOffWallClawShut) {
+            if (robot.robot.state.intake.foldIntakeBeforeRetraction.getElapsedTimeSeconds() > 2.6) {
+                robot.robot.state.intake.foldIntakeBeforeRetraction.resetTimer();
+            }
+            robot.robot.state.intake.intakeState = IntakeState.Transfer;
+        }
     }
 
     @Override
     public boolean isFinished() {
-        return robot.isIntakeExtended();
+        boolean sampleDone = (robot.intakeTimer.getElapsedTimeSeconds() > 1.0 && robot.robot.state.intake.intakeState == IntakeState.Grabbing);
+        boolean specimenDone = (robot.intakeTimer.getElapsedTimeSeconds() > 2.0 && robot.robot.state.intake.intakeState == IntakeState.ExtendedGrabbingOffWallClawShut);
+        if (sampleDone || specimenDone) {
+            if (robot.robot.state.intake.foldIntakeBeforeRetraction.getElapsedTimeSeconds() > 2.6) {
+                robot.robot.state.intake.foldIntakeBeforeRetraction.resetTimer();
+            }
+            robot.robot.state.intake.intakeState = IntakeState.Transfer;
+            return true;
+        }
+        return false;
     }
 }
