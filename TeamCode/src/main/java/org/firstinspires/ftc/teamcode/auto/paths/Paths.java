33680c5ccd0f00347aeff3416075929f3f79494e
Harrison Chapman <hchap1@hotmail.com>
2024-11-26 14:02:40 +1000

    fixed 4 spec

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/Paths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/Paths.java
@@ -12,9 +12,9 @@ public class Paths {
     public static PathChain fiveSpecimen_outtake(int s) { return FiveSpecimen.outtake(s); }
     public static PathChain fiveSpecimen_driveOntoSpecimen = FiveSpecimen.driveOntoSpecimen();
 
-    public static PathChain fourSpecimen_initial = FiveSpecimen.initial_dump();
-    public static PathChain fourSpecimen_pushes = FiveSpecimen.pushes();
-    public static PathChain fourSpecimen_intake(int s) { return FiveSpecimen.intake(s); }
-    public static PathChain fourSpecimen_outtake(int s) { return FiveSpecimen.outtake(s); }
-    public static PathChain fourSpecimen_driveOntoSpecimen = FiveSpecimen.driveOntoSpecimen();
+    public static PathChain fourSpecimen_initial = FourPlusZero.initial_dump();
+    public static PathChain fourSpecimen_pushes = FourPlusZero.pushes();
+    public static PathChain fourSpecimen_intake(int s) { return FourPlusZero.intake(s); }
+    public static PathChain fourSpecimen_outtake(int s) { return FourPlusZero.outtake(s); }
+    public static PathChain fourSpecimen_driveOntoSpecimen = FourPlusZero.driveOntoSpecimen();
 }
